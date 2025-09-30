0c8e5d717287c6b292568fd4f5f8c025dc0204fa
Nguyen Trung Kien <kiennt.ce190036@gmail.com>
2025-09-30 14:47:49 +0700

    updated

---
+++ src/main/java/vn/edu/fpt/musicstore/controller/HomeController.java
@@ -0,0 +1,12 @@
+package vn.edu.fpt.musicstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/home")
+    public String homePage() {
+        return "home/welcome";
+    }
+}
