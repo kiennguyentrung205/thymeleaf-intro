dfaca5e7cc4bc5696500f8749282f71002fab9e0
Nguyen Trung Kien <kiennt.ce190036@gmail.com>
2025-09-30 21:12:15 +0700

    updated

--- src/main/java/vn/edu/fpt/musicstore/controller/HomeController.java
+++ src/main/java/vn/edu/fpt/musicstore/controller/HomeController.java
@@ -1,12 +1,27 @@
 package vn.edu.fpt.musicstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/home")
-    public String homePage() {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String formPage(Model model) {
+        model.addAttribute("title", "Login");
+        return "login/form";
+    }
+
+    @RequestMapping(value = "/home", method = RequestMethod.POST)
+    public String homePage(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "password") String password,
+            Model model) {
+        model.addAttribute("title", "Welcom to Spring");
+        model.addAttribute("name", name);
         return "home/welcome";
     }
 }
